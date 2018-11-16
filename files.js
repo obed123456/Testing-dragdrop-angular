79cfe77c830043e948311bd10e5ff88cd76f9506
Birdvision <birdvision@Birdvisions-iMac.local>
2018-11-16 05:59:54 +0100

    add JSON with file data

---
+++ files.js
@@ -0,0 +1,32 @@
+[
+    {
+        "ID": 1, 
+        "FileName": "test.pdf", 
+        "FolderPath": 1234567890, 
+        "FileSize": 2334, 
+        "Date": 14525523, 
+        "LastModified": 12314124, 
+        "Permission": "Read Only", 
+        "UserName" : "Obed Samuel" 
+    }, 
+    {
+        "ID": 2, 
+        "FileName": "test.pdf", 
+        "FolderPath": 1234567890, 
+        "FileSize": 2334, 
+        "Date": 14525523, 
+        "LastModified": 12314124, 
+        "Permission": "Delete", 
+        "UserName" : "John Doe" 
+    },
+    {
+        "ID": 3, 
+        "FileName": "test.pdf", 
+        "FolderPath": 1234567890, 
+        "FileSize": 2334, 
+        "Date": 14525523, 
+        "LastModified": 12314124, 
+        "Permission": "Delete", 
+        "UserName" : "Rikard Karlsson" 
+    }
+]
