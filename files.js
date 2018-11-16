dbf275f97efe34133f6502c40604437b7c36c3ff
obed123456 <31744395+obed123456@users.noreply.github.com>
2018-11-16 11:07:19 +0100

    Update files.js

--- files.js
+++ files.js
@@ -28,5 +28,15 @@
         "LastModified": 12314124, 
         "Permission": "Delete", 
         "UserName" : "Rikard Karlsson" 
+    }, 
+        {
+        "ID": 4, 
+        "FileName": "test.pdf", 
+        "FolderPath": 1234567890, 
+        "FileSize": 2334, 
+        "Date": 14525523, 
+        "LastModified": 12314124, 
+        "Permission": "Delete", 
+        "UserName" : "User4" 
     }
 ]
